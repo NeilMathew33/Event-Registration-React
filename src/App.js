3271e59ec4bca31b7efdb0cf92cf90d093bc9e4f
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-19 20:03:45 -0700

    added signup page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import SearchParticipant from './container/SearchParticipant';
 import ViewAllParticipants from './container/ViewAllParticipants';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './container/Login';
+import SignUp from './container/SignUp';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Route path="/delete" element={<DeleteParticipant />} />
       <Route path="/ViewAll" element={<ViewAllParticipants />} />
       <Route path="/" element={<Login />} />
+      <Route path="/SignUp" element={<SignUp />} />
     </Routes>
     </BrowserRouter>
   );
