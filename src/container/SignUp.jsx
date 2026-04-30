330d148006a148645d48d2fed75c5df7633b542c
NeilMathew33 <mathew.neil33@gmail.com>
2026-04-29 22:37:12 -0700

    added input

--- src/container/SignUp.jsx
+++ src/container/SignUp.jsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignUp = () => {
+
+const [input, setInput] = useState(
+    {
+      firstname: "",
+      middlename: "",
+      lastname: "",
+      addressofevent: "",
+      addressofhome: "",
+      event: "",
+      time: "",
+      age: "",
+      password: "",
+      confirmpassword: "",
+    }
+  )
+  
+  const handleInput = (event) => {
+    setInput(
+      {... input,[event.target.name]:event.target.value}
+    )
+  }
+
+  const readValues = () => {
+    console.log(input)
+  }
+
   return (
     <div>
 
@@ -17,27 +43,28 @@ const SignUp = () => {
 
         <div className="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
           <label htmlFor="" className="form-label">First Name</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="firstname" value={input.firstname} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
           <label htmlFor="" className="form-label">Middle Name</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="middlename" value={input.middlename} onChange={handleInput}/>
         </div>
         <div className="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
           <label htmlFor="" className="form-label">Last Name</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="lastname" value={input.lastname} onChange={handleInput}/>
         </div>
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <label htmlFor="" className="form-label">Address of Event</label>
-         <textarea name="" id="" className="form-control"></textarea>
+         <textarea name="addressofevent" id="" className="form-control" value={input.addressofevent} onChange={handleInput}></textarea>
         </div>
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <label htmlFor="" className="form-label">Address of Home</label>
-          <textarea name="" id="" className="form-control"></textarea>
+          <textarea name="addressofhome" id="" className="form-control" value={input.addressofhome} onChange={handleInput}></textarea>
         </div>
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <label htmlFor="" className="form-label">Event</label>
-          <select name="" id="" className="form-control">
+          <select name="event" id="" className="form-control" value={input.event} onChange={handleInput}>
+            <option value="">Select Event</option>
             <option value="100M">100M</option>
             <option value="200M">200M</option>
             <option value="400M">400M</option>
@@ -45,23 +72,23 @@ const SignUp = () => {
           </select>
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-          <label htmlFor="" className="form-label">Gender</label>
-          <input type="text" className="form-control" />
+          <label htmlFor="" className="form-label">Time</label>
+          <input type="text" className="form-control" name="time" value={input.time} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Age</label>
-          <input type="text" className="form-control" />
+          <input type="text" className="form-control" name="age" value={input.age} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Password</label>
-          <input type="password" name="" id="" className="form-control" />
+          <input type="password" name="password" id="" className="form-control" value={input.password} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
           <label htmlFor="" className="form-label">Confirm Password</label>
-          <input type="password" name="" id="" className="form-control" />
+          <input type="password" name="confirmpassword" id="" className="form-control" value={input.confirmpassword} onChange={handleInput} />
         </div>
         <div className="col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-          <button className="btn btn-success">Sign Up</button>
+          <button onClick={readValues} className="btn btn-success">Sign Up</button>
         </div>
         <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
           <a href="/"> I already have an account</a>
